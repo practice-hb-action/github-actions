5ed0642b18b343bdbf424ba5f83db66948e628ed
practice-hb <chb2890@naver.com>
2026-06-28 17:22:06 +0900

    Update link text in App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn GithubAction cicd 2
+          Learn GithubAction cicd1 test
         </a>
       </header>
     </div>
